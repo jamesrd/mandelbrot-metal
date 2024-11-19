85e16316bf4b2c28fde35fa6f4c44856b39f7f0d
James DeVries <jamesrdevries@gmail.com>
2024-11-18 19:31:38 -0600

    Implement scrolling and zooming

--- Mandelbrot-Metal/MandelbrotView.swift
+++ Mandelbrot-Metal/MandelbrotView.swift
@@ -16,6 +16,29 @@ class MandelbrotView: MTKView {
             return
         }
         renderer.offset.scale = renderer.offset.scale * 0.90
+        print(renderer.offset.scale)
+        self.draw()
+    }
+    
+    override func rightMouseDown(with event: NSEvent) {
+        guard let renderer = mandelbrot else {
+            return
+        }
+        renderer.offset.scale = renderer.offset.scale * 1.10
+        print(renderer.offset.scale)
+        self.draw()
+    }
+    
+    override func scrollWheel(with event: NSEvent) {
+        guard var offset = mandelbrot?.offset else {
+            return
+        }
+        let m = offset.scale * 0.01
+        offset.x = offset.x + (Float(event.scrollingDeltaX) * m * -1)
+        offset.y = offset.y + (Float(event.scrollingDeltaY) * m)
+        print(offset.x)
+        print(offset.y)
+        mandelbrot?.offset = offset
         self.draw()
     }
     
